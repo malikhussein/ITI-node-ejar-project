c96e1ffe92e84edb4ea2693e3a5de54300cf3967
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-03-20 15:17:22 +0200

    updated category routes

--- routes/category.routes.js
+++ routes/category.routes.js
@@ -1,14 +1,31 @@
 import { Router } from "express";
 
-
-
 const categoryRoutes = Router();
 
 
-categoryRoutes.get('/', (req, res) => {
-  res.send('Category route');
+//create category
+categoryRoutes.post("/create", (req, res) => {
+  res.send("Create Category route");
 });
 
+//get all categories
+categoryRoutes.get("/all", (req, res) => {
+  res.send("All Categories route");
+});
 
+//get single category
+categoryRoutes.get("/:id", (req, res) => {
+  res.send("Single Category route");
+});
+
+//update category
+categoryRoutes.put("/:id", (req, res) => {
+  res.send("Update Category route");
+});
+
+//delete category
+categoryRoutes.delete('/:id', (req, res) => {
+  res.send('Delete Category route');
+});
 
 export default categoryRoutes;
