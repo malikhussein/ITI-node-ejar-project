d99488048a9079996fb23a49b7f5b78be1502eef
Test0-VC <omarabdin1@gmail.com>
2025-03-24 00:59:00 +0200

    Auth and User Routes

--- routes/server.routes.js
+++ routes/server.routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import categoryRoutes from './category.routes.js';
+import authRoutes from './auth.routes.js';
+import userRoutes from './user.routes.js';
 
 const mainRouter = Router();
 
-
-
-mainRouter.use('/category', categoryRoutes)
+mainRouter.use('/category', categoryRoutes);
+mainRouter.use('/auth', authRoutes);
+mainRouter.use('/user', userRoutes);
 
 export default mainRouter;
