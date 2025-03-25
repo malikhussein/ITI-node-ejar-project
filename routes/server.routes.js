29ef71386c36b973cc661de69153858b6257b760
Malik <malikhussein229@gmail.com>
2025-03-25 17:16:09 +0200

    Added the process routes to the server

--- routes/server.routes.js
+++ routes/server.routes.js
@@ -3,6 +3,7 @@ import categoryRoutes from './category.routes.js';
 import authRoutes from './auth.routes.js';
 import userRoutes from './user.routes.js';
 import Productrouter from './product.routes.js';
+import processRouter from './process.routes.js';
 
 const mainRouter = Router();
 mainRouter.use("/product", Productrouter);
@@ -10,5 +11,6 @@ mainRouter.use("/product", Productrouter);
 mainRouter.use('/category', categoryRoutes);
 mainRouter.use('/auth', authRoutes);
 mainRouter.use('/user', userRoutes);
+mainRouter.use('/process', processRouter);
 
 export default mainRouter;
