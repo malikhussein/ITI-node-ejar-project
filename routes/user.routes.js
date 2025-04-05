1f0ddb8cb438c07aeddc1016b46f2ce8908dda9d
Test0-VC <omarabdin1@gmail.com>
2025-04-05 20:18:15 +0200

    added both routes for the updateProfile picture put ==> upload get ==> to fetch the photo

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,24 @@
 import { Router } from 'express';
 import authMiddleware from '../middleware/auth.middleware.js';
-import { getAllUsers, getUserById, updateUser, deleteAllUsers, deleteUser, searchUser,} from '../controllers/user.controller.js';
+import {
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteAllUsers,
+  deleteUser,
+  searchUser,
+} from '../controllers/user.controller.js';
+import uploadProfilePicture from '../middleware/profilePictureUpload.middleware.js';
+import {
+  getProfilePicture,
+  updateProfilePicture,
+} from '../controllers/updateProfilePicture.controller.js';
 const userRoutes = Router();
 
+// Route to update user profile picture
+// This route is protected by the authMiddleware, meaning only logged-in users can access it as the rest
+userRoutes.put('/upload-profile-picture',authMiddleware, uploadProfilePicture.single('profilePicture'),updateProfilePicture);
+userRoutes.get('/profile-picture', authMiddleware, getProfilePicture);
 userRoutes.get('/', authMiddleware, getAllUsers);
 userRoutes.get('/:id', authMiddleware, getUserById);
 userRoutes.post('/search', authMiddleware, searchUser);
