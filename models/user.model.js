ba5a4f0dce9486541c01d33b53bc99e71d41ebdc
Test0-VC <omarabdin1@gmail.com>
2025-04-05 20:17:28 +0200

    added profilePicture to user model schema with a default static avatar photo

--- models/user.model.js
+++ models/user.model.js
@@ -25,6 +25,9 @@ const userSchema = new Schema({
     enum: Object.values(roleType), // Retrieve all values from the roleType object
     default: roleType.user,
   }, 
+  profilePicture: { type: String, default: "https://static.vecteezy.com/system/resources/previews/026/619/142/non_2x/default-avatar-profile-icon-of-social-media-user-photo-image-vector.jpg" ,
+  },
+
   
   // If you want to store file paths for images:
   idPictureFrontPath: {
