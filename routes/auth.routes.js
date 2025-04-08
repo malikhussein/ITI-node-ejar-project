f0c77c7ed1d46147d8f170749548e2b5f88d338b
ahmed <ahmedamro3000@gmail.com>
2025-04-08 04:12:57 +0200

    ID Photo

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,36 +1,27 @@
-import { Router } from 'express';
-import * as authRoutesService from '../controllers/auth.controller.js';
-import { validation } from '../middleware/joi.middleware.js';
-import { signInJoiSchema, signUpJoiSchema } from '../config/joi.validation.js';
-import { forgotPassword} from "../controllers/forgotPassword.controller.js";
-import { resetPassword} from "../controllers/resetPassword.controller.js";
-import upload from '../middleware/multer.middleware.js';
-import { cleanupUploadedFiles } from '../middleware/cleanUploadedFiles.middleware.js';
+import { Router } from "express";
+import * as authRoutesService from "../controllers/auth.controller.js";
+import { validation } from "../middleware/joi.middleware.js";
+import { signInJoiSchema, signUpJoiSchema } from "../config/joi.validation.js";
+import { forgotPassword } from "../controllers/forgotPassword.controller.js";
+import { resetPassword } from "../controllers/resetPassword.controller.js";
+import { cleanupUploadedFiles } from "../middleware/cleanUploadedFiles.middleware.js";
+import { upload } from "../controllers/product.controller.js";
 
 const authRoutes = Router();
 
 authRoutes.post(
-  '/signUp',
-  upload.fields([
-    { name: 'idPictureFront', maxCount: 1 },
-    { name: 'idPictureBack', maxCount: 1 },
-  ]),
-  cleanupUploadedFiles,        //  CLEANUP for Uploaded files (Must come before validation)
+  "/signUp",
+  upload,
+
   validation(signUpJoiSchema), // Validate request
-  authRoutesService.register   // Handle registration
+  authRoutesService.register // Handle registration
 );
-authRoutes.post(
-  '/login',
-  validation(signInJoiSchema), 
-  authRoutesService.login
-);
+authRoutes.post("/login", validation(signInJoiSchema), authRoutesService.login);
 // verify email
-authRoutes.get('/verify/:token', authRoutesService.verifyEmail);
+authRoutes.get("/verify/:token", authRoutesService.verifyEmail);
 
 //  resetpassword flow
 authRoutes.post("/forgot-password", forgotPassword);
-authRoutes.post("/reset-password/:token", resetPassword); 
-
-
+authRoutes.post("/reset-password/:token", resetPassword);
 
 export default authRoutes;
