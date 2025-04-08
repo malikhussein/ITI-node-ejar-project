8662b8e5828e4893542887626eb8e5e86e280611
Malik <malikhussein229@gmail.com>
2025-04-08 15:01:58 +0200

    Added a new route to get the chat by id

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -58,6 +58,51 @@ export default class Chat {
     }
   }
 
+  static async getChatById(req, res) {
+    try {
+      // * Check if request is authenticated
+      if (!req.user) {
+        return res.status(401).json({ message: 'no token provided' });
+      }
+
+      const { id: userId, role: userRole } = req.user;
+
+      // * Check if the user exists
+      const user = await userModel.findById(userId);
+
+      if (!user) {
+        return res.status(404).json({ message: 'user does not exist' });
+      }
+
+      const { chatId } = req.params;
+
+      // add the userpicture
+      const chat = await chatModel
+        .findById(chatId)
+        .populate('members', '_id userName');
+
+      // * Check if there is any chats
+      if (!chat) {
+        return res.status(404).json({ message: 'no chats were found' });
+      }
+
+      // * Check if the user is a member of the chat
+      if (
+        !chat.members.some(
+          (member) => member._id.toString() === userId.toString()
+        )
+      ) {
+        return res.status(403).json({ message: 'not authorized' });
+      }
+
+      return res.status(200).json(chat);
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ message: error.message || 'internal server error' });
+    }
+  }
+
   static async getUserChats(req, res) {
     try {
       // * Check if request is authenticated
