bb7eb353c0cb7fa002026487f6f507da5baa21da
Malik <malikhussein229@gmail.com>
2025-04-13 14:50:39 +0200

    Added the auth middleware to the edit product and delete

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { Router } from 'express';
 import {
   addProduct,
   getAllProducts,
@@ -6,17 +6,23 @@ import {
   removeProduct,
   updateProduct,
   upload,
-} from "../controllers/product.controller.js";
-import authMiddleware from "../middleware/auth.middleware.js";
-import { validation } from "../middleware/joi.middleware.js";
-import { ProductSchema } from "../config/joi.product.validation.js";
+} from '../controllers/product.controller.js';
+import authMiddleware from '../middleware/auth.middleware.js';
+import { validation } from '../middleware/joi.middleware.js';
+import { ProductSchema } from '../config/joi.product.validation.js';
 
 const Productrouter = Router();
 
-Productrouter.post("/", authMiddleware, upload, validation(ProductSchema), addProduct);
-Productrouter.post("/:id", upload, updateProduct);
-Productrouter.get("/:id", getOneProduct);
-Productrouter.get("/", getAllProducts);
-Productrouter.delete("/:id", removeProduct);
+Productrouter.post(
+  '/',
+  authMiddleware,
+  upload,
+  validation(ProductSchema),
+  addProduct
+);
+Productrouter.post('/:id', authMiddleware, upload, updateProduct);
+Productrouter.get('/:id', getOneProduct);
+Productrouter.get('/', getAllProducts);
+Productrouter.delete('/:id', authMiddleware, removeProduct);
 
 export default Productrouter;
